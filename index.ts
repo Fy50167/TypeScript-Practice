6d4fd9c7a73de5bf68dc6024ae3d1ec24f173f21
Francis Yang <fy50167@gmail.com>
2024-02-13 15:27:01 -0800

    defining primitives

--- index.ts
+++ index.ts
@@ -1,3 +1,31 @@
 let firstname: string = "This is my string!";
 
 let age: string | number;
+
+let array: (string | number | boolean)[] = ['string', 100, true];
+
+let tuple: [string, number, boolean] = ['string', 100, true];
+
+let object: {
+    string: string;
+    number: number;
+    boolean: boolean;
+}
+
+object = {
+    string: 'string',
+    number: 100,
+    boolean: true
+}
+
+interface Interface {
+    string: string;
+    number: number;
+    boolean: boolean;
+}
+
+let newObject: Interface = {
+    string: 'string',
+    number: 100,
+    boolean: true
+}
